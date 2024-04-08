3180c56a6f2fa58067f29493ccfa15c47bb10b5f
Roba Nath Basnet <robanbth@gmail.com>
2024-04-08 13:23:58 +0600

    fix

--- dist/src/index.js
+++ dist/src/index.js
@@ -8,37 +8,35 @@ const tokens_json_1 = __importDefault(require("../tokens.json"));
 const express_rate_limit_1 = require("express-rate-limit");
 const token_1 = require("./token");
 const ethers_1 = require("ethers");
-require("dotenv/config");
 const app = (0, express_1.default)();
-const port = process.env.port;
+const port = 8080;
 const limiter = (0, express_rate_limit_1.rateLimit)({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 15, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     // store: ... , // Redis, Memcached, etc. See below.
 });
 app.use(limiter);
-app.get("/", (req, res) => {
-    res.send("Hello, TypeScript Express!");
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript Express!');
 });
 // get circulating supply
-app.get("/:ticker/circulating", async (req, res) => {
+app.get('/:ticker/circulating', async (req, res) => {
     try {
         let { ticker } = req.params;
         let token = tokens_json_1.default[ticker];
         if (!token)
-            return res.status(400).json({
-                error: "400",
-                message: "token ticker not support.Is case sensitive make sure is match",
+            return res.status(400).json({ error: "400",
+                message: "token ticker not support.Is case sensitive make sure is match"
             });
         let _circulating = await (0, token_1.circulating_supply)(token);
-        return res
-            .status(200)
-            .json({ circulating_supply: ethers_1.ethers.formatEther(_circulating) });
+        return res.status(200).json({ "circulating_supply": ethers_1.ethers.formatEther(_circulating) });
     }
     catch (err) {
-        return res.status(400).json({ error: "400", message: `ERROR: ${err}` });
+        return res.status(400).json({ error: "400",
+            message: `ERROR: ${err}`
+        });
     }
 });
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,44 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
 import tokens from "../tokens.json";
-import { rateLimit } from "express-rate-limit";
-import { circulating_supply, Token } from "./token";
-import { ethers } from "ethers";
-import "dotenv/config";
-
+import { rateLimit } from 'express-rate-limit'
+import { circulating_supply, Token } from './token';
+import { ethers } from 'ethers';
 const app: Application = express();
-const port = process.env.port;
+const port = 8080;
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 15, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
-});
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit:15, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
-app.use(limiter);
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+app.use(limiter)
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
 });
 
 // get circulating supply
-app.get("/:ticker/circulating", async (req: Request, res: Response) => {
-  try {
-    let { ticker } = req.params;
-    let token: Token | undefined = tokens[
-      ticker as keyof typeof tokens
-    ] as unknown as Token;
-    if (!token)
-      return res.status(400).json({
-        error: "400",
-        message:
-          "token ticker not support.Is case sensitive make sure is match",
-      });
-    let _circulating = await circulating_supply(token);
+app.get('/:ticker/circulating',async(req:Request,res:Response)=>{
+  try{
+    let {ticker} = req.params
+    let token:Token | undefined = tokens[ticker as keyof typeof tokens] as unknown as Token
+    if(!token) return res.status(400).json(
+        {error:"400",
+         message:"token ticker not support.Is case sensitive make sure is match"   
+        })
+    let _circulating  = await circulating_supply(token);
 
-    return res
-      .status(200)
-      .json({ circulating_supply: ethers.formatEther(_circulating) });
-  } catch (err) {
-    return res.status(400).json({ error: "400", message: `ERROR: ${err}` });
+    return res.status(200).json({"circulating_supply":ethers.formatEther(_circulating)})
+  }catch(err){
+    return res.status(400).json(
+      {error:"400",
+       message:`ERROR: ${err}`   
+      })
   }
-});
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
